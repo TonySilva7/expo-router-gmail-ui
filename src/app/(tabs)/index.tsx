86892e17e94a1a2f80b9484f0e56de63374ece4b
TonySilva7 <tony.deve7oper@gmail.com>
2024-07-21 11:37:02 -0300

    chore: create project

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, ViewProps, StyleSheet } from 'react-native';
+
+type HomeProps = ViewProps
+
+export default function Home({...props}: HomeProps) {
+
+  return(
+    <View style={styles.container} {...props}>
+      <Text>Hello Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
